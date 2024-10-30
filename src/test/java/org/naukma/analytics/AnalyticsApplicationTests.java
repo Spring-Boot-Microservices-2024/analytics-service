1e01b1ba4996862361daade1c90d80234b67562c
Svitlana-Marchenko <sveta25022004@gmail.com>
2024-10-31 00:07:19 +0200

    feat: add eureka and Feign

--- src/test/java/org/naukma/analytics/AnalyticsApplicationTests.java
+++ src/test/java/org/naukma/analytics/AnalyticsApplicationTests.java
@@ -1,13 +1,10 @@
 package org.naukma.analytics;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AnalyticsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class AnalyticsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
